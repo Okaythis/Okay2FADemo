b4b8d92f7d12e93001e008cd69036cee07fc6882
Ben Ogie <bensoft2k5@gmail.com>
2020-05-07 12:16:59 +0200

    added login route

---
+++ app/src/controllers/auth/login/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/', (req, res) => {
+
+    // verify user credentials
+    // then send second auth
+});
+
+export default router;
